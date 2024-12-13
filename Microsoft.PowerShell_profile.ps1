669bfdcaa35b7564047e48bc39d7eec87e77ce6c
Justin <46439389+JustinBrow@users.noreply.github.com>
2024-12-13 10:48:08 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+$PSDefaultParameterValues=@{'Invoke-WebRequest:UserAgent'='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'}
+
+[enum]::GetNames([Net.SecurityProtocolType]) | % {[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_}
+
+if (-not (Get-PSDrive -Name HKU -ErrorAction SilentlyContinue))
+{
+  [void](New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
+}
+
+$TypeData = @{
+   TypeName   = [System.Diagnostics.Process].ToString()
+   MemberType = [System.Management.Automation.PSMemberTypes]::ScriptProperty
+   MemberName = 'CommandLine'
+   Value = {(Get-CimInstance Win32_Process -Filter "ProcessId = $($this.Id)").CommandLine}
+}
+Update-TypeData @TypeData
